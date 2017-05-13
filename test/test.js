8f65264ac970905c9acdd540328ade999cefd859
Jose <josenaveen.a.alphonse@oracle.com>
2017-05-13 10:53:37 +0530

    Added Tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,10 @@
 /**
  * http://usejsdoc.org/
  */
+var expect = require("chai").expect;
+
+describe("SAMPLE unit test",function(){
+	it("Dummy test",function(){
+		expect(1+1).to.equal(2)
+	});
+});
